2a561424521ad4c2e4ac5fe4a67bd6ade8de57e7
Max Willsey <me@mwillsey.com>
2020-09-09 12:07:50 -0700

    Generate results in one go

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use egg::*;
-use std::time::Instant;
+use std::time::{Instant, Duration};
 
 define_language! {
     enum Lang {
@@ -8,27 +8,44 @@ define_language! {
     }
 }
 
-fn main() {
-    let w = 2000;
-    let d = 100;
-    let mut egraph: EGraph<Lang, ()> = EGraph::new(());
-    let mut xs = vec![];
-    for i in 1..w {
-        let x = format!("x_{}", i);
-        let mut id = egraph.add_expr(&x.parse().unwrap());
-        xs.push(id);
-        for _j in 1..d {
-            // let f = format!("f_{}", j);
-            // id = egraph.add(Language::from_op_str(&f, vec![id]).unwrap());
-            id = egraph.add(Language::from_op_str("f", vec![id]).unwrap());
+fn go(upward: bool) {
+    let mut w = 100;
+    loop {
+        let d = 100;
+        let mut egraph: EGraph<Lang, ()> = EGraph::new(());
+        let mut xs = vec![];
+        for i in 1..w {
+            let x = format!("x_{}", i);
+            let mut id = egraph.add_expr(&x.parse().unwrap());
+            xs.push(id);
+            for _j in 1..d {
+                // let f = format!("f_{}", j);
+                // id = egraph.add(Language::from_op_str(&f, vec![id]).unwrap());
+                id = egraph.add(Language::from_op_str("f", vec![id]).unwrap());
+            }
+        }
+
+        let start = Instant::now();
+        for i in 1..w - 1 {
+            egraph.union(xs[0], xs[i]);
+            if upward {
+                egraph.rebuild();
+            }
+        }
+        if !upward {
+            egraph.rebuild();
+        }
+
+        let time = start.elapsed();
+        println!("{}, {}", w, time.as_millis());
+        w += 100;
+        if time > Duration::from_secs(2) || w >= 10000{
+            break
         }
     }
+}
 
-    let start = Instant::now();
-    for i in 1..w-1 {
-        egraph.union(xs[0], xs[i]);
-        // egraph.rebuild();
-    }
-    egraph.rebuild();
-    println!("{}", start.elapsed().as_millis());
+fn main() {
+    go(true);
+    go(false);
 }
